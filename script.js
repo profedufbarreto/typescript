04aa08f8977c194b26e202bab13edfe466632020
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-25 19:45:33 -0300

    do 0

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Olá");
+let num1 = 4;
+let num2 = 3;
+let soma = num1 + num2;
+console.log(`O resultado da soma é ${soma}`);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,7 @@
-console.log("Olá");
+let num1: number = 4;
+let num2: number = 3;
+
+let soma: number = num1 + num2;
+
+
+console.log(`O resultado da soma é ${soma}`);
