3556ea40534a225aef6489963e3d96a713281a9a
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-25 19:47:41 -0300

    if else

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
-let num1: number = 4;
-let num2: number = 3;
+let idade: number = 18;
 
-let soma: number = num1 + num2;
-
-
-console.log(`O resultado da soma é ${soma}`);
+if(idade >= 18){
+    console.log("Maior de idade!");
+}else{
+    console.log("Menor de idade!");
+}
