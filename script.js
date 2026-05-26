ae34a4a8e010911b3fb29722b769a2bf973286e5
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-26 18:31:23 -0300

    trocando o título

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Pessoa {
-    nome;
-    idade;
-    constructor(nome, idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-    apresentar() {
-        return `${this.nome} (${this.idade} anos)`;
-    }
-}
-let pessoas = [];
-function adicionarPessoa() {
-    let inputNome = document.getElementById('inputNome');
-    let inputIdade = document.getElementById('inputIdade');
-    let nome = inputNome.value;
-    let idade = parseInt(inputIdade.value);
-    if (nome === "" || isNaN(idade)) {
-        alert("Preencha todos os campos!");
-        return;
-    }
-    let p = new Pessoa(nome, idade);
-    pessoas.push(p);
-    inputNome.value = "";
-    inputIdade.value = "";
-    exibirPessoas();
-}
-function exibirPessoas() {
-    let lista = document.getElementById('lista');
-    lista.innerHTML = "";
-    pessoas.forEach((p, index) => {
-        let div = document.createElement('div');
-        div.className = 'pessoa';
-        div.innerHTML = `
-            <strong>${p.apresentar()}</strong>
-            <button onclick="removerPessoa(${index}")>Remover</button>
-            `;
-        lista.appendChild(div);
-    });
-}
-function removerPessoa(index) {
-    pessoas.splice(index, 1);
-    exibirPessoas();
+function mudarTitulo() {
+    let titulo = document.getElementById('titulo');
+    titulo.innerText = "Título mudou!";
 }
 //# sourceMappingURL=script.js.map
