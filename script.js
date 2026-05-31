94531ad51fb500a349955301f4a21546bc1163aa
profedu_fbarreto <seu_email@github.com>
2026-05-31 13:52:01 -0300

    spread operator

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
+//===== SPREAD OPERATOR =====//
 Object.defineProperty(exports, "__esModule", { value: true });
-let [a, b, c] = [1, 2, 3];
-console.log(a);
-let pessoa = { nome: "Eduardo", idade: 38 };
-let { nome, idade } = pessoa;
-console.log(`Eu me chamo ${nome} e tenho ${idade} anos.`);
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let junto = [...arr1, ...arr2];
+console.log(`Array 1: ${arr1}`);
+console.log(`Array 2: ${arr2}`);
+console.log(`Array juntos: ${junto}`);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,8 +1,10 @@
-let [a, b, c] = [1, 2, 3];
+//===== SPREAD OPERATOR =====//
 
-console.log(a);
+let arr1: number[] = [1, 2, 3];
+let arr2: number[] = [4, 5, 6];
 
-let pessoa = {nome: "Eduardo", idade: 38};
-let {nome, idade} = pessoa;
+let junto: number[] = [...arr1, ...arr2];
 
-console.log(`Eu me chamo ${nome} e tenho ${idade} anos.`);
+console.log(`Array 1: ${arr1}`);
+console.log(`Array 2: ${arr2}`);
+console.log(`Array juntos: ${junto}`);
