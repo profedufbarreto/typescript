a37deda75b3245e6e3cc075e00e61cda81f81875
profedu_fbarreto <seu_email@github.com>
2026-05-31 13:44:57 -0300

    destructuring

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function mudarTitulo() {
-    let titulo = document.getElementById('titulo');
-    titulo.innerText = "Título mudou!";
-}
+let [a, b, c] = [1, 2, 3];
+console.log(a);
+let pessoa = { nome: "Eduardo", idade: 38 };
+let { nome, idade } = pessoa;
+console.log(`Eu me chamo ${nome} e tenho ${idade} anos.`);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,4 +1,8 @@
-function mudarTitulo(): void{
-    let titulo = document.getElementById('titulo');
-    titulo!.innerText = "Título mudou!";
-}
+let [a, b, c] = [1, 2, 3];
+
+console.log(a);
+
+let pessoa = {nome: "Eduardo", idade: 38};
+let {nome, idade} = pessoa;
+
+console.log(`Eu me chamo ${nome} e tenho ${idade} anos.`);
