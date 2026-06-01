95ea1e337f02e45be1749768101c579bf96fc415
profedu_fbarreto <seu_email@github.com>
2026-05-31 21:09:47 -0300

    Encapsulamento

--- script.js
+++ script.js
@@ -7,16 +7,28 @@ class StudentWithoutEncapsulation {
         this.name = name;
         this.grade = grade;
     }
+    getGrade() {
+        return this.grade;
+    }
+    setGrade(value) {
+        if (value >= 0 && value <= 10) {
+            this.grade = value;
+            console.log(`Grade set to ${value}`);
+        }
+        else {
+            console.log(`Invalid grade! Must be between 0 and 10`);
+        }
+    }
     showGrade() {
         console.log(`${this.name} grade: ${this.grade}`);
     }
 }
-let student1 = new StudentWithoutEncapsulation("Eduardo", 8.5);
-student1.showGrade();
-student1.grade = 10;
-student1.showGrade();
-student1.grade = 999;
-student1.showGrade();
-student1.grade = -9;
-student1.showGrade();
+let student2 = new StudentWithoutEncapsulation("Eduardo", 8.5);
+student2.showGrade();
+student2.setGrade(10);
+student2.showGrade();
+student2.setGrade(999);
+student2.showGrade();
+student2.setGrade(-5);
+student2.showGrade();
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,25 +1,38 @@
 class StudentWithoutEncapsulation{
-    name: string;
-    grade: number;
+    private name: string;
+    private grade: number;
 
     constructor(name: string, grade: number){
         this.name = name;
         this.grade = grade;
     }
 
-    showGrade(): void{
+    public getGrade(): number{
+        return this.grade;
+    }
+
+    public setGrade(value: number): void{
+        if(value >= 0 && value <= 10){
+            this.grade = value;
+            console.log(`Grade set to ${value}`);
+        }else{
+            console.log(`Invalid grade! Must be between 0 and 10`);
+        }
+    }
+
+    public showGrade(): void{
         console.log(`${this.name} grade: ${this.grade}`);
     }
 }
 
-let student1 = new StudentWithoutEncapsulation("Eduardo", 8.5);
-student1.showGrade();
+let student2 = new StudentWithoutEncapsulation("Eduardo", 8.5);
+student2.showGrade();
 
-student1.grade = 10;
-student1.showGrade();
+student2.setGrade(10);
+student2.showGrade();
 
-student1.grade = 999;
-student1.showGrade();
+student2.setGrade(999);
+student2.showGrade();
 
-student1.grade = -9;
-student1.showGrade();
+student2.setGrade(-5);
+student2.showGrade();
