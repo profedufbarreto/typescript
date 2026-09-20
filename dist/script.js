36b091b7d88a69d496c6c008352902b2681c7831
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-19 22:09:03 -0300

    calculadora com interface e classe

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,108 @@
 "use strict";
-const uniqueKey = Symbol('description');
-const obj = {
-    [uniqueKey]: 'This is a unique property'
-};
-console.log(obj[uniqueKey]);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline = __importStar(require("readline"));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+class Calculadora {
+    a;
+    b;
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    somar() {
+        return this.a + this.b;
+    }
+    subtrair() {
+        return this.a - this.b;
+    }
+    multiplicar() {
+        return this.a * this.b;
+    }
+    dividir() {
+        if (this.b === 0)
+            throw new Error("Não é possível dividir por 0!");
+        return this.a / this.b;
+    }
+}
+function iniciar() {
+    rl.question("Digite o valor de a (ou 'sair' para encerrar): ", (aStr) => {
+        if (aStr.toLowerCase() === "sair") {
+            console.log("Encerrando calculadora...");
+            rl.close();
+            return;
+        }
+        rl.question("Digite o valor de b: ", (bStr) => {
+            rl.question("Escolha a operação (+, -, *, /): ", (op) => {
+                const a = Number(aStr);
+                const b = Number(bStr);
+                if (isNaN(a) || isNaN(b)) {
+                    console.error("Erro: você deve digitar números válidos!");
+                    return iniciar();
+                }
+                const calc = new Calculadora(a, b);
+                try {
+                    let resultado;
+                    switch (op) {
+                        case "+":
+                            resultado = calc.somar();
+                            break;
+                        case "-":
+                            resultado = calc.subtrair();
+                            break;
+                        case "*":
+                            resultado = calc.multiplicar();
+                            break;
+                        case "/":
+                            resultado = calc.dividir();
+                            break;
+                        default:
+                            throw new Error("Operação inválida!");
+                    }
+                    console.log(`Resultado: ${resultado}`);
+                }
+                catch (error) {
+                    console.error("Erro: ", error.message);
+                }
+                iniciar();
+            });
+        });
+    });
+}
+iniciar();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,85 @@
-const uniqueKey: symbol = Symbol('description');
+import { bytesSync } from "node:stream/iter";
+import * as readline from "readline";
 
-const obj = {
-    [uniqueKey] : 'This is a unique property'
-};
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(obj[uniqueKey]);
+class Calculadora{
+    a: number;
+    b: number;
+
+    constructor(a: number, b: number){
+        this.a = a;
+        this.b = b;
+    }
+
+    somar(): number{
+        return this.a + this.b;
+    }
+
+    subtrair(): number{
+        return this.a - this.b;
+    }
+
+    multiplicar(): number{
+        return this.a * this.b;
+    }
+
+    dividir(): number{
+        if(this.b === 0) throw new Error("Não é possível dividir por 0!");
+        return this.a / this.b;
+    }
+}
+
+function iniciar(){
+    rl.question("Digite o valor de a (ou 'sair' para encerrar): ", (aStr) => {
+        if(aStr.toLowerCase() === "sair"){
+            console.log("Encerrando calculadora...");
+            rl.close();
+            return;
+        }
+
+        rl.question("Digite o valor de b: ", (bStr) => {
+            rl.question("Escolha a operação (+, -, *, /): ", (op) => {
+                const a = Number(aStr);
+                const b = Number(bStr);
+
+                if(isNaN(a) || isNaN(b)){
+                    console.error("Erro: você deve digitar números válidos!");
+                    return iniciar();
+                }
+
+                const calc = new Calculadora(a, b);
+
+                try{
+                    let resultado: number;
+                    switch(op){
+                        case "+":
+                            resultado = calc.somar();
+                            break;
+                        case "-":
+                            resultado = calc.subtrair();
+                            break;
+                        case "*":
+                            resultado = calc.multiplicar();
+                            break;
+                        case "/":
+                            resultado = calc.dividir();
+                            break;
+                        default:
+                            throw new Error("Operação inválida!");
+                    }
+                        console.log(`Resultado: ${resultado}`);
+                    }catch(error){
+                        console.error("Erro: ", (error as Error).message);
+                    }   
+
+                    iniciar();
+            });
+        });
+    });
+}
+
+iniciar();
