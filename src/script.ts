0f0a21ab61100fa5a52de5645b3a37ae2dd846bd
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-19 22:31:19 -0300

    class

--- src/script.ts
+++ src/script.ts
@@ -1,85 +1,21 @@
-import { bytesSync } from "node:stream/iter";
-import * as readline from "readline";
+class Animal{
+    nome: string;
+    idade: number = 3;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-class Calculadora{
-    a: number;
-    b: number;
-
-    constructor(a: number, b: number){
-        this.a = a;
-        this.b = b;
+    constructor(nome: string){
+        this.nome = nome;
+        this.idade = this.idade;
     }
 
-    somar(): number{
-        return this.a + this.b;
+    falar(): void{
+        console.log(`${this.nome} está fazendo um som...`);
     }
 
-    subtrair(): number{
-        return this.a - this.b;
-    }
-
-    multiplicar(): number{
-        return this.a * this.b;
-    }
-
-    dividir(): number{
-        if(this.b === 0) throw new Error("Não é possível dividir por 0!");
-        return this.a / this.b;
+    apresentar(): void{
+        console.log(`${this.nome} tem ${this.idade} anos!`);
     }
 }
 
-function iniciar(){
-    rl.question("Digite o valor de a (ou 'sair' para encerrar): ", (aStr) => {
-        if(aStr.toLowerCase() === "sair"){
-            console.log("Encerrando calculadora...");
-            rl.close();
-            return;
-        }
-
-        rl.question("Digite o valor de b: ", (bStr) => {
-            rl.question("Escolha a operação (+, -, *, /): ", (op) => {
-                const a = Number(aStr);
-                const b = Number(bStr);
-
-                if(isNaN(a) || isNaN(b)){
-                    console.error("Erro: você deve digitar números válidos!");
-                    return iniciar();
-                }
-
-                const calc = new Calculadora(a, b);
-
-                try{
-                    let resultado: number;
-                    switch(op){
-                        case "+":
-                            resultado = calc.somar();
-                            break;
-                        case "-":
-                            resultado = calc.subtrair();
-                            break;
-                        case "*":
-                            resultado = calc.multiplicar();
-                            break;
-                        case "/":
-                            resultado = calc.dividir();
-                            break;
-                        default:
-                            throw new Error("Operação inválida!");
-                    }
-                        console.log(`Resultado: ${resultado}`);
-                    }catch(error){
-                        console.error("Erro: ", (error as Error).message);
-                    }   
-
-                    iniciar();
-            });
-        });
-    });
-}
-
-iniciar();
+const cachorro = new Animal("Rex");
+cachorro.falar();
+cachorro.apresentar();
