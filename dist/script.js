9e7503296bd5ac192028263500eb8e09bbcad724
profefbarreto <01289211027@senacrs.edu.br>
2026-09-21 08:36:52 -0300

    arrays

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,17 @@
 "use strict";
-const numeros = [1, 2, 3, 4, 5];
-const dobrados = numeros.map(n => n * 2);
-console.log(dobrados);
+const soma = (a, b) => a + b;
+const media = (numeros) => {
+    const total = numeros.reduce((acc, n) => acc + n, 0);
+    return total / numeros.length;
+};
+const maiorNumero = (numeros) => Math.max(...numeros);
+const menorNumero = (numeros) => Math.min(...numeros);
+const dobrarNumeros = (numeros) => numeros.map(n => n * 2);
+//Teste das funções
+const valores = [10, 20, 30, 40, 50];
+console.log("Soma: ", soma(5, 7));
+console.log("Média: ", media(valores));
+console.log("Maior nùmero: ", maiorNumero(valores));
+console.log("Menos número: ", menorNumero(valores));
+console.log("Dobrar números: ", dobrarNumeros(valores));
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,22 @@
-const numeros = [1, 2, 3, 4, 5];
-const dobrados = numeros.map(n => n * 2);
+const soma = (a: number, b: number): number => a + b;
 
-console.log(dobrados);
+const media = (numeros: number[]): number =>{
+    const total = numeros.reduce((acc, n) => acc + n, 0);
+    return total / numeros.length;
+}
+
+const maiorNumero = (numeros: number[]): number => Math.max(...numeros);
+
+const menorNumero = (numeros: number[]): number => Math.min(...numeros);
+
+const dobrarNumeros = (numeros: number[]): number[] => numeros.map(n => n * 2);
+
+//Teste das funções
+
+const valores = [10, 20, 30, 40, 50];
+
+console.log("Soma: ", soma(5, 7));
+console.log("Média: ", media(valores));
+console.log("Maior nùmero: ", maiorNumero(valores));
+console.log("Menos número: ", menorNumero(valores));
+console.log("Dobrar números: ", dobrarNumeros(valores));
