e7703c1f25ca7b0dff101b206bd61bf0a05b6727
profefbarreto <01289211027@senacrs.edu.br>
2026-09-21 08:38:13 -0300

    usando reduce

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,5 @@
 "use strict";
-const soma = (a, b) => a + b;
-const media = (numeros) => {
-    const total = numeros.reduce((acc, n) => acc + n, 0);
-    return total / numeros.length;
-};
-const maiorNumero = (numeros) => Math.max(...numeros);
-const menorNumero = (numeros) => Math.min(...numeros);
-const dobrarNumeros = (numeros) => numeros.map(n => n * 2);
-//Teste das funções
-const valores = [10, 20, 30, 40, 50];
-console.log("Soma: ", soma(5, 7));
-console.log("Média: ", media(valores));
-console.log("Maior nùmero: ", maiorNumero(valores));
-console.log("Menos número: ", menorNumero(valores));
-console.log("Dobrar números: ", dobrarNumeros(valores));
+const numeros = [1, 2, 3, 4, 5];
+const soma = numeros.reduce((acc, n) => acc + n, 0);
+console.log(soma);
 //# sourceMappingURL=script.js.map
