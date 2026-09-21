1e91f8289b793352ab91d815f204c0a088cac320
profefbarreto <01289211027@senacrs.edu.br>
2026-09-21 09:30:30 -0300

    manipulando dados

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,37 @@
-const letras = ["a", "b", "a", "c", "b", "a"];
+interface Cliente{
+    id: number;
+    nome: string;
+    idade: number;
+}
 
-const contagem = letras.reduce((acc, letra) => {
-    acc[letra] = (acc[letra] || 0) + 1;
-    return acc;
-}, {} as Record<string, number>);
+let clientes: Cliente[] = [];
 
-console.log(contagem);
+const inserirCliente = (cliente: Cliente): void =>{
+    clientes.push(cliente);
+    console.log("Cliente inserido: ", cliente);
+};
+
+const listarClientes = (): void =>{
+    console.log("Lista de clientes: ", clientes);
+};
+
+const atualizarCliente = (id: number, novosDados: Partial<Cliente>): void => {
+    clientes = clientes.map(cliente =>
+        cliente.id === id ? { ...cliente, ...novosDados } : cliente
+    );
+    console.log(`Cliente ${id} atualizado.`);
+};
+
+const deletarCliente = (id: number): void => {
+    clientes = clientes.filter(cliente => cliente.id !== id);
+    console.log(`Cliente ${id} removido.`);
+};
+
+inserirCliente({id: 1, nome: "Eduardo", idade: 30});
+inserirCliente({id: 2, nome: "Maria", idade: 25});
+inserirCliente({id: 3, nome: "João", idade: 40});
+
+listarClientes();
+
+atualizarCliente(2, {idade: 26, nome: "Maria Silva"});
+listarClientes();
