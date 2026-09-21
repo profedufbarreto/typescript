48cc19bdc89afdf40d0d4cb6874b82af8627026b
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-20 23:07:42 -0300

    function

--- dist/script.js
+++ dist/script.js
@@ -1,38 +1,6 @@
 "use strict";
-class FormaGeometrica {
-    nome;
-    constructor(nome) {
-        this.nome = nome;
-    }
-    info() {
-        console.log(`Forma: ${this.nome}, Área: ${this.calcularArea()}`);
-    }
+function saudacao() {
+    console.log("Olá, mundo!");
 }
-class Quadrado extends FormaGeometrica {
-    lado;
-    constructor(lado) {
-        super("Quadrado");
-        this.lado = lado;
-    }
-    calcularArea() {
-        return this.lado * this.lado;
-    }
-}
-class Circulo extends FormaGeometrica {
-    raio;
-    constructor(raio) {
-        super("Círculo");
-        this.raio = raio;
-    }
-    calcularArea() {
-        return Math.PI * this.raio * this.raio;
-    }
-}
-const formas = [
-    new Quadrado(4),
-    new Circulo(3)
-];
-for (const f of formas) {
-    f.info();
-}
+saudacao();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,48 +1,5 @@
-abstract class FormaGeometrica {
-    protected nome: string;
-
-    constructor(nome: string){
-        this.nome = nome;
-    }
-
-    abstract calcularArea(): number;
-
-    info(): void{
-        console.log(`Forma: ${this.nome}, Área: ${this.calcularArea()}`);
-    }
+function saudacao(){
+    console.log("Olá, mundo!");
 }
 
-class Quadrado extends FormaGeometrica{
-    private lado: number;
-
-    constructor(lado: number){
-        super("Quadrado");
-        this.lado = lado;
-    }
-
-    calcularArea(): number{
-        return this.lado * this.lado;
-    }
-}
-
-class Circulo extends FormaGeometrica{
-    private raio: number;
-
-    constructor(raio: number){
-        super("Círculo");
-        this.raio = raio;
-    }
-
-    calcularArea(): number {
-        return Math.PI * this.raio * this.raio;
-    }
-}
-
-const formas: FormaGeometrica[] = [
-    new Quadrado(4),
-    new Circulo(3)
-];
-
-for(const f of formas){
-    f.info();
-}
+saudacao();
